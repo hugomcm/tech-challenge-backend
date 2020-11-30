2e9235f9cf53ebcf3ca2dc7cf062795b4c554f02
Hugo Monteiro <hugomcm@gmail.com>
2020-11-30 12:10:51 +0000

    Genre and Actor CRUD relations (w/transactions)

--- src/lib/movies.ts
+++ src/lib/movies.ts
@@ -6,14 +6,35 @@ export interface Movie {
   synopsis: string, 
   releasedAt: Date, 
   runtime: number
+  actors: MovieActor[],
+  genres: MovieGenre[]
 }
+
+export interface MovieActor {
+  actor_id: number,
+  character_name: string
+}
+export interface MovieGenre {
+  genre_id: number
+}
+
 
 export function list(): Promise<Movie[]> {
   return knex.from('movie').select()
 }
 
 export function find(id: number): Promise<Movie> {
-  return knex.from('movie').where({ id }).first()
+  // return knex.from('movie').where({ id }).first()
+  return Promise.all([
+    knex.from('movie').where({ id }).first(),
+    knex.from('actor_movie').where({ movie_id: id }).select('actor_id', 'character_name'),
+    knex.from('genre_movie').where({ movie_id: id }).select('genre_id')
+  ])
+    .then(([ movie, movieActors, movieGenres ]) => {
+      if(!movie) throw(`No movie with id: ${id}`)
+      return { ...movie, actors: movieActors, genres: movieGenres }
+    })
+    .catch((err) => undefined)
 }
 
 /** @returns whether the ID was actually found */
@@ -23,13 +44,39 @@ export async function remove(id: number): Promise<boolean> {
 }
 
 /** @returns the ID that was created */
-export async function create(name: string, synopsis: string, releasedAt: Date, runtime: number): Promise<number> {
-  const [ id ] = await (knex.into('movie').insert({ name, synopsis, releasedAt, runtime }))
-  return id
+export async function create(name: string, synopsis: string, releasedAt: Date, runtime: number, actors: MovieActor[], genres: MovieGenre[]): Promise<number> {
+  // const [ id ] = await (knex.into('movie').insert({ name, synopsis, releasedAt, runtime }))
+  // return id
+  return await knex.transaction(async trx => {    
+    const [ id ] = await (trx.into('movie').insert({ name, synopsis, releasedAt, runtime }))
+    if(!!actors && actors instanceof Array){
+      await knex.into('actor_movie').insert(actors.map(aa => ({ movie_id: id, ...aa }))).transacting(trx)
+    }
+    if(!!genres && genres instanceof Array){
+      await knex.into('genre_movie').insert(genres.map(ag => ({ movie_id: id, ...ag }))).transacting(trx)
+    }
+
+    return id
+  })
 }
 
 /** @returns whether the ID was actually found */
-export async function update(id: number, name: string, synopsis: string, releasedAt: Date, runtime: number): Promise<boolean>  {
-  const count = await knex.from('movie').where({ id }).update({ name, synopsis, releasedAt, runtime })
-  return count > 0
+export async function update(id: number, name: string, synopsis: string, releasedAt: Date, runtime: number, actors: MovieActor[], genres: MovieGenre[]): Promise<boolean>  {
+  // const count = await knex.from('movie').where({ id }).update({ name, synopsis, releasedAt, runtime })
+  // return count > 0
+  return await knex.transaction(async trx => { 
+    const count = await knex.from('movie').where({ id }).update({ name, synopsis, releasedAt, runtime })
+
+    if(!!actors && actors instanceof Array) {
+      await knex.from('actor_movie').where({ movie_id: id }).delete().transacting(trx) 
+      await knex.into('actor_movie').insert(actors.map(aa => ({ movie_id: id, ...aa }))).transacting(trx)
+    }
+
+    if(!!genres && genres instanceof Array){
+      await knex.from('genre_movie').where({ movie_id: id }).delete().transacting(trx) 
+      await knex.into('genre_movie').insert(genres.map(ag => ({ movie_id: id, ...ag }))).transacting(trx)
+    }
+
+    return count > 0
+  })
 }
--- src/plugins/movies/routes.ts
+++ src/plugins/movies/routes.ts
@@ -26,7 +26,9 @@ interface PayloadMovie {
   name: string, 
   synopsis: string, 
   releasedAt: Date, 
-  runtime: number
+  runtime: number,
+  actors: movies.MovieActor[],
+  genres: movies.MovieGenre[]
 }
 const validatePayloadMovie: RouteOptionsResponseSchema = {
   payload: joi.object({
@@ -34,6 +36,8 @@ const validatePayloadMovie: RouteOptionsResponseSchema = {
     synopsis: joi.string(),
     releasedAt: joi.date().required(),
     runtime: joi.number().required(),
+    actors: joi.array(),
+    genres: joi.array()
   })
 }
 
@@ -77,10 +81,10 @@ async function get(req: Request, _h: ResponseToolkit, _err?: Error): Promise<Lif
 }
 
 async function post(req: Request, h: ResponseToolkit, _err?: Error): Promise<Lifecycle.ReturnValue> {
-  const { name, synopsis, releasedAt, runtime } = (req.payload as PayloadMovie)
+  const { name, synopsis, releasedAt, runtime, actors, genres } = (req.payload as PayloadMovie)
 
   try {
-    const id = await movies.create(name, synopsis, releasedAt, runtime)
+    const id = await movies.create(name, synopsis, releasedAt, runtime, actors, genres)
     const result = {
       id,
       path: `${req.route.path}/${id}`
@@ -88,21 +92,35 @@ async function post(req: Request, h: ResponseToolkit, _err?: Error): Promise<Lif
     return h.response(result).code(201)
   }
   catch(er: unknown){
-    if(!isHasCode(er) || er.code !== 'ER_DUP_ENTRY') throw er
-    return Boom.conflict()
+    if(isHasCode(er)){
+      if(er.code === 'ER_DUP_ENTRY'){
+        return Boom.conflict()
+      }
+      else if(er.code === 'ER_NO_REFERENCED_ROW_2'){
+        return Boom.preconditionFailed()
+      }
+    }
+    throw er;
   }
 }
 
 async function put(req: Request, h: ResponseToolkit, _err?: Error): Promise<Lifecycle.ReturnValue> {
   const { id } = (req.params as ParamsId)
-  const { name, synopsis, releasedAt, runtime } = (req.payload as PayloadMovie)
+  const { name, synopsis, releasedAt, runtime, actors, genres } = (req.payload as PayloadMovie)
 
   try {
-    return await movies.update(id, name, synopsis, releasedAt, runtime) ? h.response().code(204) : Boom.notFound()
+    return await movies.update(id, name, synopsis, releasedAt, runtime, actors, genres) ? h.response().code(204) : Boom.notFound()
   }
   catch(er: unknown){
-    if(!isHasCode(er) || er.code !== 'ER_DUP_ENTRY') throw er
-    return Boom.conflict()
+    if(isHasCode(er)){
+      if(er.code === 'ER_DUP_ENTRY'){
+        return Boom.conflict()
+      }
+      else if(er.code === 'ER_NO_REFERENCED_ROW_2'){
+        return Boom.preconditionFailed()
+      }
+    }
+    throw er;
   }
 }
 
